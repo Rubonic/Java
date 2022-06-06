20ca9079e53d13488e1033833ce635cf2c86c114
Rubonic <dannykraemer099@gmail.com>
2022-06-06 12:42:55 -0400

    first commit

---
+++ Foundations/First_Java_Program/FirstJavaProgram.java
@@ -0,0 +1,7 @@
+public class FirstJavaProgram {
+    public static void main(String[] args) {
+        System.out.println("My name is Daniel Kraemer");
+        System.out.println("I am 24 years old");
+        System.out.println("My home town is Fort Myers, FL");
+    }
+}
