3bd9c8ad7e88dc5026451d300d0c792bb001f055
Rubonic <dannykraemer099@gmail.com>
2022-06-08 12:03:02 -0400

    first commit

---
+++ OOP/ZooKeeper/Part1/GorillaTest.java
@@ -0,0 +1,15 @@
+public class GorillaTest {
+    public class GorillaTest {
+        public static void main(String args[]){
+            Gorilla g = new Gorilla();
+            
+            g.throwItem();
+            g.throwItem();
+            g.throwItem();
+            g.eatBananas();
+            g.eatBananas();
+            g.climb();
+            g.displayEnergy();
+        }
+    }
+}
