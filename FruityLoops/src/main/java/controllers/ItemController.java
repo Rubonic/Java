deab803d126261c048573593c0cd9adfe1bbf400
Rubonic <dannykraemer099@gmail.com>
2022-06-11 09:59:55 -0400

    fist commit

--- FruityLoops/src/main/java/controllers/ItemController.java
+++ FruityLoops/src/main/java/controllers/ItemController.java
@@ -1,5 +1,24 @@
 package controllers;
 
-public class ItemController {
+import java.util.ArrayList;
 
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import models.Item;
+
+@RestController
+public class ItemController {
+	@RequestMapping("/")
+	public String index(Model model) {
+		ArrayList<Item> fruitsArrayList = new ArrayList<Item>();
+		fruitsArrayList.add(new Item("kiwi", 1.5));
+		fruitsArrayList.add(new Item("Mango", 2.0));
+		fruitsArrayList.add(new Item("Bayas de Goyi", 4.0));
+		fruitsArrayList.add(new Item("Guayava", 75));
+		
+		model.addAttribute("fruits",fruitsArrayList);
+		return "index.jsp";
+	}
 }
