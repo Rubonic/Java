557348749772eb4a264280e69b759265a917fb5b
Rubonic <dannykraemer099@gmail.com>
2022-06-21 10:29:03 -0400

    first commit

--- Algorithm/Week3Day1.js
+++ Algorithm/Week3Day1.js
@@ -1,22 +1,35 @@
-class MinHeap {
-    constructor() {
-        this.data = [null, 10, 40, 20, 60, 50, 30, 70];
+class MinHeap
+{
+    constructor()
+    {
+        this.data=[null,10,40,20,60,50,30,70];
     }
 
     /* Description: Inserts a value into the heap, maintaining order. */
     /* Instructions: */
-    /* 1. push the value to the end of the array.*/
-    /* 2. Iteratively swap the new number with its parent value while it is */
-    /* smaller than it's parent. */
+        /* 1. push the value to the end of the array.*/
+        /* 2. Iteratively swap the new number with its parent value while it is */
+        /* smaller than it's parent. */
     /* Time: O(log n) */
     /* Space: O(1) */
-    Insert(value) {
+    Insert(value)
+    {
         /* Your Code Here */
-        for(let i = this.data.length; i > 1; i >>= 1){
-            if(this.data[i] < this.data[i >> 1]){
-                let temp = this.data[i];
-                this.data[i] = this.data[i >> 1];
-                this.data[i >> 1] = temp;
+        let len=this.data.length;
+        this.data[len]=value;
+        let parent=Math.floor((len-1)/2);
+        for(;parent>=0;parent--)
+        {
+            if(this.data[parent]>this.data[len])
+            {
+                let temp=this.data[parent];
+                this.data[parent]=this.data[len];
+                this.data[len]=temp;
+                len=parent;
+            }
+            else
+            {
+                break;
             }
         }
     }
@@ -24,25 +37,24 @@ class MinHeap {
     /* Description: Returns the smallest value in the heap without removing it. */
     /* Time: O(1) */
     /* Space: O(1) */
-    Min() {
+    Min()
+    {
         /* Your Code Here */
-        for(let i = 1; i < this.data.length; i <<= 1){
-            if(this.data[i] < this.data[i + 1]){
-                i++;
-            }
-        }
         return this.data[1];
     }
 
-    Log() {
-        let fmt = "";
-        let len = this.data.length - 1;
-        let tabs = 0;
-        for (; len; len >>= 1, tabs++) { }
-        for (let i = 1; i < this.data.length; i <<= 1, tabs--) {
-            let to = (i << 1) - 1;
+    Log()
+    {
+        let fmt="";
+        let len=this.data.length-1;
+        let tabs=0;
+        for(;len;len>>=1,tabs++){}
+        for(let i=1;i<this.data.length;i<<=1,tabs--)
+        {
+            let to=(i<<1)-1;
 
-            for (let j = 0; j < to; j++) {
+            for(let j=0;j<to;j++)
+            {
 
             }
         }
@@ -50,7 +62,7 @@ class MinHeap {
     }
 };
 
-let heap = new MinHeap();
+let heap=new MinHeap();
 heap.Insert(5);
 heap.Log(); /* Expected: 5,10,40,20,60,50,30,70 */
 console.log(heap.Min()); /* Expected: 5 */
